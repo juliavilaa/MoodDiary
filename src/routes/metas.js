4d1971bff0752456184f745d195d06bd4e1b33f6
Zierk110 <109629901+Zierk110@users.noreply.github.com>
2024-11-03 01:25:32 -0500

    Quinto commit
    Se modificó el archivo routes/metas

--- src/routes/metas.js
+++ src/routes/metas.js
@@ -13,8 +13,46 @@ router.post("/", (req, res) => {
 module.exports = router;
 //Consultar todos las metas
 router.get("/",(req, res) => {
-  metaslSchema
+  metasSchema
     .find()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
+
+//Consultar meta por du id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    metasSchema
+      .findById(id)
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+  
+  //Modificar una emocion por su id
+  router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    metasSchema
+      .updateOne(
+        { _id: id },
+        {
+          $set: { titulo, descripcion, fechaInicio, fechaFinalizacion, estado },
+        }
+      )
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+  
+  //Eliminar una emocion por su id
+  
+  router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    metasSchema
+      .findByIdAndDelete(id)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
