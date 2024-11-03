4feb99d6f40bba0e6e1b73cfe5d7bbd6ac3ea554
Laura-Vanessa <jvanessarincon13@gmail.com>
2024-11-02 23:52:22 -0500

    Tercer commit emociones
    Se agregan los atributos de emociones en el models/emociones

---
+++ src/models/Emociones.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const emocionesSchema = mongoose.Schema({
+
+    nombreEmocion: {
+        type: String,
+        required: true,
+    },
+    
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    
+    fecha: {
+        type: Date,
+        requiered: true,
+    }
+});
+module.exports = mongoose.model("Emociones", emocionesShema);
